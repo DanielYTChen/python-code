2fb152d0ee8dde4d074f38d70773a78e8e5d2dba
Daniel <triumph910102@gmail.com>
2018-04-27 17:25:20 +0800

    1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+print("Hello","Hello")
+str = "I am learning programmong."
+print(str)
+print(len(str))
+print(str[2])
+print(str[3:8])
+print(str[:13])
+print(str[2:])
+print(str[:])
+print(str[::-1])
+
+
+print{"{0} {1}".format(100,200)}
+print{"num:{0} num:{1}".format(a,b)}
+print{"{0:10}{1:10}".format(100,200)}
+
+s="stone campus"
+print(len(s))
