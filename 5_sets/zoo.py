ed92acc2c9d295de09d6cadb2eb1cc5bae3ca8c2
Daniel <triumph910102@gmail.com>
2018-05-25 17:23:09 +0800

    my first file

---
+++ 5_sets/zoo.py
@@ -0,0 +1,13 @@
+student=[2000,19000,1800,215] #list
+ages={
+    'Daniel':25,    #dict
+}
+
+zooA = {'elephant', 'tiger', 'lion', 'giraffe', 'leopard', 'antelop'}  #sets
+zooB = {'lion', 'cheetah', 'giraffe', 'zebra', 'wildebeest', 'elephant'}
+print("zoo A:",zooA)
+print("zoo B:",zooB)
+print("zoo A and zoo B:",zooA & zooB)
+print("zoo A or zoo B:",zooA | zooB)
+print("zoo A - zoo B:",zooA - zooB)
+print("zoo A xor zoo B:",zooA ^ zooB)
